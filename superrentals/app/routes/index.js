30cb74acc0e26cf6c6c290e9744ebc462df04fc0
kannan-santharam <kannan.santharam@freshworks.com>
2021-03-01 08:57:43 +0530

    tutorial app.- part 1 completed

--- superrentals/app/routes/index.js
+++ superrentals/app/routes/index.js
@@ -1,9 +1,24 @@
 import Route from '@ember/routing/route';
-
+const COMMUNITY_CATEGORIES = [
+    'Condo',
+    'Townhouse',
+    'Apartment'
+];
 export default class IndexRoute extends Route {
-  async model() {
-   let response = await fetch("/api/rentals.json");
-   let rentalsData = await response.json();
-   return rentalsData;
-  }
+    async model() {
+        let response = await fetch("/api/rentals.json");
+        let { data } = await response.json();
+        return data.map(model => {
+            let { attributes } = model;
+            let type;
+            
+            if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+                type = 'Community';
+            } else {
+                type = 'Standalone';
+            }
+            console.info({ type, ...attributes })
+            return { type, ...attributes };
+        });
+    }
 }
